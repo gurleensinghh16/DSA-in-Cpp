66c52ffb50f90d3b3a5036fb14af404a970c02cc
Gurleen-2007 <sgurllen@gmail.com>
2026-01-09 22:21:47 +0530

    solved Two sum problem in dsa

---
+++ two_sum_1.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int n, target;
+
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    vector<int> nums(n);
+    cout << "Enter elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> nums[i];
+    }
+
+    cout << "Enter target: ";
+    cin >> target;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (nums[i] + nums[j] == target) {
+                cout << "Indices: " << i << " " << j << endl;
+                return 0;
+            }
+        }
+    }
+
+    cout << "No two sum solution found" << endl;
+    return 0;
+}
