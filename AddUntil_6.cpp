b3154eae7ab56766a063a909b5a2f573680d9e3a
gurleensinghh16 <gurleensinghh16@gmail.com>
2026-01-14 21:36:11 +0530

    Add numbers continously in integer

---
+++ AddUntil_6.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+    int num,sum=0,sum2=0;
+    cout<<"Enter the number : "<<endl;
+    cin>>num;
+    while(num>0){
+        sum+=num%10;
+        num=num/10;
+    }
+    cout<<"1st sum is : "<<sum<<endl;
+    while(sum>0){
+        sum2+=sum%10;
+        sum=sum/10;
+    }
+    cout<<"Sum is : "<<sum2<<endl;
+    return 0;
+}
+// Add integer until it lefts in one diigit
