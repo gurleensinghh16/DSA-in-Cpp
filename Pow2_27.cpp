54edac44c100ef2e76ea5f1aecea12500f2a28b3
gurleensinghh16 <gurleensinghh16@gmail.com>
2026-02-06 19:56:24 +0530

    check if number is power pf 2 or not optimal :

---
+++ Pow2_27.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+    int n;
+    long x=2;
+    bool st;
+    cout<<"Enter the number you want to check power of 2 or not :\n";
+    cin>>n;
+    if(n==1){
+        st=true;
+        cout<<st;
+        return 0;
+    }
+    while(x<=INT_MAX){
+        if(n==x){
+            st=true;
+            cout<<st;
+            return 0;
+        }
+        x=x*2;
+    }
+    st=false;
+    cout<<st;
+    return 0;
+}
+// Check power of 2 or not :
